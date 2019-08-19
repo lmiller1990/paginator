6128f18c8512a7983a8ad8d90d38595bd5974123
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-08-19 19:48:39 +1000

    init commit w/ basic implementation

---
+++ index.spec.ts
@@ -0,0 +1,133 @@
+import { paginate, IPageNode, TNode } from './paginate'
+
+interface IListArgs {
+  totalItems: number
+}
+
+interface IItem {
+  id: number
+}
+
+const createList = ({ totalItems }: IListArgs): IItem[] => {
+  const arr = []
+  for (let i = 0; i < totalItems; i++) {
+    arr.push({ id: i })
+  }
+  return arr
+}
+
+describe('less than 5 pages', () => {
+  it('shows all pages', () => {
+    const list = createList({ totalItems: 50 })
+    const actual = paginate(list, { currentPage: 1 }, { perPage: 10 })
+    const expected: TNode[] = [
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 2 },
+      { type: 'pageNumber', value: 3 },
+      { type: 'pageNumber', value: 4 },
+      { type: 'pageNumber', value: 5 },
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' }
+    ]
+
+    expect([ actual[0], actual[1] ]).toEqual([ 
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+    ])
+
+
+    expect([ actual[7], actual[8] ]).toEqual([ 
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' },
+    ])
+  })
+})
+
+describe('exactly 7 pages', () => {
+  it('shows all pages', () => {
+    const list = createList({ totalItems: 70 })
+    const actual = paginate(list, { currentPage: 1 }, { perPage: 10 })
+    const expected: TNode[] = [
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 2 },
+      { type: 'pageNumber', value: 3 },
+      { type: 'pageNumber', value: 4 },
+      { type: 'pageNumber', value: 5 },
+      { type: 'pageNumber', value: 6 },
+      { type: 'pageNumber', value: 7 },
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' }
+    ]
+
+    expect([ actual[0], actual[1] ]).toEqual([ 
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+    ])
+
+    expect(actual[5]).toEqual({ type: 'pageNumber', value: 4 })
+
+
+    expect([ actual[9], actual[10] ]).toEqual([ 
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' },
+    ])
+  })
+})
+
+describe.only('more than 7 pages', () => {
+  it('shows dots to hide pages', () => {
+    const list = createList({ totalItems: 1000 })
+    const currentPage = 1
+    const actual = paginate(list, { currentPage }, { perPage: 10 })
+
+    // Case 3.1: [1, 2, 3, 4, 5, 6, 7, 8 ..... 1000] (1000 pages)
+    // Current page is 1
+    // << | < | 1 | 2 | 3 | ... | 98 | 99 | 100 | > | >>
+    const expected: TNode[] = [
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 2 },
+      { type: 'pageNumber', value: 3 },
+      { type: 'dots', value: '...' },
+      { type: 'pageNumber', value: 98 },
+      { type: 'pageNumber', value: 99 },
+      { type: 'pageNumber', value: 100 },
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' }
+    ]
+
+    expect([ actual[0], actual[1] ]).toEqual([ 
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+    ])
+
+    expect([ actual[2], actual[3], actual[4] ]).toEqual([
+      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 2 },
+      { type: 'pageNumber', value: 3 },
+    ])
+
+    expect([ actual[5] ]).toEqual([
+      { type: 'dots', value: '...' }
+    ])
+
+    expect([ actual[6], actual[7], actual[8] ]).toEqual([
+      { type: 'pageNumber', value: 98 },
+      { type: 'pageNumber', value: 99 },
+      { type: 'pageNumber', value: 100 },
+    ])
+
+
+    expect([ actual[9], actual[10] ]).toEqual([ 
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' },
+    ])
+  })
+})
