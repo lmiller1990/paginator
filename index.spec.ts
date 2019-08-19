9e851836a8a5c8c37f5b6c5b406e3911b5842220
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-08-19 20:31:46 +1000

    Cover all cases

--- index.spec.ts
+++ index.spec.ts
@@ -24,12 +24,12 @@ describe('less than 5 pages', () => {
     const expected: TNode[] = [
       { type: 'navigation', value: '<<' },
       { type: 'navigation', value: '<' },
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 2 },
-      { type: 'pageNumber', value: 3 },
-      { type: 'pageNumber', value: 4 },
-      { type: 'pageNumber', value: 5 },
+      { type: 'pageNumber', value: 1, isCurrentPage: true },
+      { type: 'pageNumber', value: 1, isCurrentPage: false },
+      { type: 'pageNumber', value: 2, isCurrentPage: false },
+      { type: 'pageNumber', value: 3, isCurrentPage: false },
+      { type: 'pageNumber', value: 4, isCurrentPage: false },
+      { type: 'pageNumber', value: 5, isCurrentPage: false },
       { type: 'navigation', value: '>' },
       { type: 'navigation', value: '>>' }
     ]
@@ -54,14 +54,14 @@ describe('exactly 7 pages', () => {
     const expected: TNode[] = [
       { type: 'navigation', value: '<<' },
       { type: 'navigation', value: '<' },
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 2 },
-      { type: 'pageNumber', value: 3 },
-      { type: 'pageNumber', value: 4 },
-      { type: 'pageNumber', value: 5 },
-      { type: 'pageNumber', value: 6 },
-      { type: 'pageNumber', value: 7 },
+      { type: 'pageNumber', value: 1, isCurrentPage: false },
+      { type: 'pageNumber', value: 1, isCurrentPage: false },
+      { type: 'pageNumber', value: 2, isCurrentPage: false },
+      { type: 'pageNumber', value: 3, isCurrentPage: false },
+      { type: 'pageNumber', value: 4, isCurrentPage: false },
+      { type: 'pageNumber', value: 5, isCurrentPage: false },
+      { type: 'pageNumber', value: 6, isCurrentPage: false },
+      { type: 'pageNumber', value: 7, isCurrentPage: false },
       { type: 'navigation', value: '>' },
       { type: 'navigation', value: '>>' }
     ]
@@ -71,7 +71,7 @@ describe('exactly 7 pages', () => {
       { type: 'navigation', value: '<' },
     ])
 
-    expect(actual[5]).toEqual({ type: 'pageNumber', value: 4 })
+    expect(actual[5]).toEqual({ type: 'pageNumber', value: 4, isCurrentPage: false })
 
 
     expect([ actual[9], actual[10] ]).toEqual([ 
@@ -81,7 +81,7 @@ describe('exactly 7 pages', () => {
   })
 })
 
-describe.only('more than 7 pages', () => {
+describe('more than 7 pages', () => {
   it('shows dots to hide pages', () => {
     const list = createList({ totalItems: 1000 })
     const currentPage = 1
@@ -93,13 +93,13 @@ describe.only('more than 7 pages', () => {
     const expected: TNode[] = [
       { type: 'navigation', value: '<<' },
       { type: 'navigation', value: '<' },
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 2 },
-      { type: 'pageNumber', value: 3 },
+      { type: 'pageNumber', value: 1, isCurrentPage: true },
+      { type: 'pageNumber', value: 2, isCurrentPage: false },
+      { type: 'pageNumber', value: 3, isCurrentPage: false },
       { type: 'dots', value: '...' },
-      { type: 'pageNumber', value: 98 },
-      { type: 'pageNumber', value: 99 },
-      { type: 'pageNumber', value: 100 },
+      { type: 'pageNumber', value: 98, isCurrentPage: false },
+      { type: 'pageNumber', value: 99, isCurrentPage: false },
+      { type: 'pageNumber', value: 100, isCurrentPage: false },
       { type: 'navigation', value: '>' },
       { type: 'navigation', value: '>>' }
     ]
@@ -110,9 +110,9 @@ describe.only('more than 7 pages', () => {
     ])
 
     expect([ actual[2], actual[3], actual[4] ]).toEqual([
-      { type: 'pageNumber', value: 1 },
-      { type: 'pageNumber', value: 2 },
-      { type: 'pageNumber', value: 3 },
+      { type: 'pageNumber', value: 1, isCurrentPage: true },
+      { type: 'pageNumber', value: 2, isCurrentPage: false },
+      { type: 'pageNumber', value: 3, isCurrentPage: false },
     ])
 
     expect([ actual[5] ]).toEqual([
@@ -120,9 +120,9 @@ describe.only('more than 7 pages', () => {
     ])
 
     expect([ actual[6], actual[7], actual[8] ]).toEqual([
-      { type: 'pageNumber', value: 98 },
-      { type: 'pageNumber', value: 99 },
-      { type: 'pageNumber', value: 100 },
+      { type: 'pageNumber', value: 98, isCurrentPage: false },
+      { type: 'pageNumber', value: 99, isCurrentPage: false },
+      { type: 'pageNumber', value: 100, isCurrentPage: false },
     ])
 
 
@@ -134,7 +134,7 @@ describe.only('more than 7 pages', () => {
 
   it('current page is in the middle', () => {
     const list = createList({ totalItems: 1000 })
-    const currentPage = 500
+    const currentPage = 50
     const actual = paginate(list, { currentPage }, { perPage: 10 })
 
     // Case 3.2: [1, 2, 3, 4, 5, 6, 7, 8 ..... 100] (100 pages)
@@ -147,7 +147,7 @@ describe.only('more than 7 pages', () => {
     ])
 
     expect(actual[2]).toEqual(
-      { type: 'pageNumber', value: 1 },
+      { type: 'pageNumber', value: 1, isCurrentPage: false },
     )
 
     expect(actual[3]).toEqual(
@@ -155,19 +155,54 @@ describe.only('more than 7 pages', () => {
     )
 
     expect([ actual[4], actual[5], actual[6] ]).toEqual([
-      { type: 'pageNumber', value: 49 },
-      { type: 'pageNumber', value: 50 },
-      { type: 'pageNumber', value: 51 },
+      { type: 'pageNumber', value: 49, isCurrentPage: false },
+      { type: 'pageNumber', value: 50, isCurrentPage: true },
+      { type: 'pageNumber', value: 51, isCurrentPage: false },
     ])
 
     expect(actual[7]).toEqual(
       { type: 'dots', value: '...' },
     )
 
-
     expect(actual[8]).toEqual(
-      { type: 'pageNumber', value: 100 },
+      { type: 'pageNumber', value: 100, isCurrentPage: false },
     )
+
+    expect([ actual[9], actual[10] ]).toEqual([ 
+      { type: 'navigation', value: '>' },
+      { type: 'navigation', value: '>>' },
+    ])
+  })
+
+  it('current page is at the end', () => {
+    const list = createList({ totalItems: 1000 })
+    const currentPage = 97
+    const actual = paginate(list, { currentPage }, { perPage: 10 })
+
+    // Case 3.4: [1, 2, 3, 4, 5, 6, 7, 8 ..... 100] (100 pages)
+    // Current page is 997
+    // << | < | 1 | ... | 96 | 97 | 98 | 99 | 100 | > | >>
+
+    expect([ actual[0], actual[1] ]).toEqual([ 
+      { type: 'navigation', value: '<<' },
+      { type: 'navigation', value: '<' },
+    ])
+
+    expect(actual[2]).toEqual(
+      { type: 'pageNumber', value: 1, isCurrentPage: false },
+    )
+
+    expect(actual[3]).toEqual(
+      { type: 'dots', value: '...' },
+    )
+
+    expect([ actual[4], actual[5], actual[6], actual[7], actual[8] ]).toEqual([
+      { type: 'pageNumber', value: 96, isCurrentPage: false },
+      { type: 'pageNumber', value: 97, isCurrentPage: true },
+      { type: 'pageNumber', value: 98, isCurrentPage: false },
+      { type: 'pageNumber', value: 99, isCurrentPage: false },
+      { type: 'pageNumber', value: 100, isCurrentPage: false },
+    ])
 
     expect([ actual[9], actual[10] ]).toEqual([ 
       { type: 'navigation', value: '>' },
--- paginate.ts
+++ paginate.ts
@@ -2,7 +2,6 @@
 // Tile count should be static
 // There should always be 11 tiles showing at max, including next, previous, last and first buttons
 
-
 // Case 1: Pages are [1,2,3,4,5] (5 or less pages)
 // Current page is 1
 // Show all pages, and navigation buttons regardless of current page
@@ -40,82 +39,108 @@
 // Current page is 1000
 // << | < | 1 | 2 | 3 | ... | 998 | 999 | 1000 | > | >>
 
-const arr = []
+const arr = [];
 for (let i = 0; i < 10000; i++) {
-  arr.push({ id: i })
+  arr.push({ id: i });
 }
 
-type TNodeType = 'dots' | 'pageNumber' | 'navigation'
+type TNodeType = "dots" | "pageNumber" | "navigation";
 
-export type TNode = IPageNumberNode | IDotsNode | INavigationNode
+export type TNode = IPageNumberNode | IDotsNode | INavigationNode;
 
 export interface IPageNode {
-  type: TNodeType
+  type: TNodeType;
 }
 
 export interface IPageNumberNode extends IPageNode {
-  type: 'pageNumber'
-  value: number
+  type: "pageNumber";
+  isCurrentPage: boolean;
+  value: number;
 }
 
 export interface IDotsNode extends IPageNode {
-  type: 'dots'
-  value: '...'
+  type: "dots";
+  value: "...";
 }
 
 export interface INavigationNode extends IPageNode {
-  type: 'navigation'
-  value: '<' | '<<' | '>' | '>>'
-}    
+  type: "navigation";
+  value: "<" | "<<" | ">" | ">>";
+}
 
-const gotoStart: INavigationNode = { type: 'navigation', value: '<<' }
-const previous: INavigationNode = { type: 'navigation', value: '<' }
+const gotoStart: INavigationNode = { type: "navigation", value: "<<" };
+const previous: INavigationNode = { type: "navigation", value: "<" };
 
-const gotoEnd: INavigationNode = { type: 'navigation', value: '>>' }
-const next: INavigationNode = { type: 'navigation', value: '>' }
+const gotoEnd: INavigationNode = { type: "navigation", value: ">>" };
+const next: INavigationNode = { type: "navigation", value: ">" };
 
 interface IPaginateOptions {
-  perPage: number
+  perPage: number;
 }
 
 interface IPaginationState {
-  currentPage: number
+  currentPage: number;
 }
 
-const paginate = (list: any[], { currentPage }: IPaginationState, { perPage }: IPaginateOptions) => {
-  const pageCount = Math.ceil(list.length / perPage)
+const paginate = (
+  list: any[],
+  { currentPage }: IPaginationState,
+  { perPage }: IPaginateOptions
+) => {
+  const makePageNumberNode = (value: number): IPageNumberNode => {
+    return {
+      type: "pageNumber",
+      value,
+      isCurrentPage: value === currentPage
+    };
+  };
 
-  const nodes: TNode[] = [gotoStart, previous]
+  const pageCount = Math.ceil(list.length / perPage);
 
+  const nodes: TNode[] = [gotoStart, previous];
+
+  // The total pages are less than 7 so no need to show '...'.
+  // Just render all pages.
   if (pageCount <= 7) {
     for (let i = 0; i < pageCount; i++) {
-      nodes.push({
-        type: 'pageNumber',
-        value: i + 1
-      })
+      nodes.push(makePageNumberNode(i + 1));
     }
-
-    return [...nodes, next, gotoEnd]
+    return [...nodes, next, gotoEnd];
   }
 
-  // first 3
-  nodes.push({ type: 'pageNumber', value: 1 })
-  nodes.push({ type: 'pageNumber', value: 2 })
-  nodes.push({ type: 'pageNumber', value: 3 })
-
-  nodes.push({ type: 'dots', value: '...' })
-
-  const nFromEnd = (count: number, n: number): number => {
-    return count - n
+  // current page is near the start
+  if (currentPage < 3) {
+    nodes.push(makePageNumberNode(1));
+    nodes.push(makePageNumberNode(2));
+    nodes.push(makePageNumberNode(3));
+    nodes.push({ type: "dots", value: "..." });
+    nodes.push(makePageNumberNode(pageCount - 2));
+    nodes.push(makePageNumberNode(pageCount - 1));
+    nodes.push(makePageNumberNode(pageCount));
+    return [...nodes, next, gotoEnd];
   }
 
-  nodes.push({ type: 'pageNumber', value: pageCount - 2 })
-  nodes.push({ type: 'pageNumber', value: pageCount - 1 })
-  nodes.push({ type: 'pageNumber', value: pageCount })
+  // current page is near the end
+  if (currentPage > pageCount - 5) {
+    nodes.push(makePageNumberNode(1));
+    nodes.push({ type: "dots", value: "..." });
+    nodes.push(makePageNumberNode(pageCount - 4));
+    nodes.push(makePageNumberNode(pageCount - 3));
+    nodes.push(makePageNumberNode(pageCount - 2));
+    nodes.push(makePageNumberNode(pageCount - 1));
+    nodes.push(makePageNumberNode(pageCount));
+    return [...nodes, next, gotoEnd];
+  }
 
-  return [...nodes, next, gotoEnd]
-}
+  // current page is "in the middle", eg not near the start or end!!
+  nodes.push(makePageNumberNode(1));
+  nodes.push({ type: "dots", value: "..." });
+  nodes.push(makePageNumberNode(currentPage - 1));
+  nodes.push(makePageNumberNode(currentPage));
+  nodes.push(makePageNumberNode(currentPage + 1));
+  nodes.push({ type: "dots", value: "..." });
+  nodes.push(makePageNumberNode(pageCount));
+  return [...nodes, next, gotoEnd];
+};
 
-export {
-  paginate
-}
+export { paginate };
